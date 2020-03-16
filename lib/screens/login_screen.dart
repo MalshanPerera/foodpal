b594de4f212d70c4a89ae1b1c08f3b3bbda4e3fd
therealsujji <sujithacrysis@gmail.com>
2020-03-16 17:04:31 +0530

    login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:foodpal/helper/utils.dart';
+import 'package:foodpal/widgets/CustomTextField.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Utils.setScreenSizes(context);
+
+    final _width = Utils.bodyWidth;
+    final _height = Utils.totalBodyHeight;
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: _width,
+          height: _height,
+          child: Image.asset(
+            'assets/images/login_background.png',
+            fit: BoxFit.fill,
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, -1),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: Utils.getDesignHeight(211)),
+                child: SizedBox(
+                  width: Utils.getDesignHeight(263),
+                  height: Utils.getDesignHeight(53),
+                  child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text("Welcome Back!",
+                          style: Theme.of(context).textTheme.title)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: Utils.getDesignHeight(0)),
+                child: SizedBox(
+                  width: Utils.getDesignHeight(179),
+                  height: Utils.getDesignHeight(27),
+                  child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text("Sign in To Continue",
+                          style: Theme.of(context).textTheme.title)),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: Utils.getDesignWidth(26),
+                      left: Utils.getDesignWidth(23),
+                      right: Utils.getDesignWidth(23)),
+                  child: CustomTextField("User Name", Icons.person)),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: Utils.getDesignWidth(26),
+                      left: Utils.getDesignWidth(23),
+                      right: Utils.getDesignWidth(23)),
+                  child: CustomTextField("Password", Icons.lock)),
+              Container(
+                margin: EdgeInsets.only(left: Utils.getDesignWidth(255),right: Utils.getDesignWidth(26)),
+                child: FittedBox(
+                    fit:  BoxFit.contain,
+                    child: Text("Forgot Passowrd",style: Theme.of(context).textTheme.title)),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
