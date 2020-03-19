d3771c984b36ba892b1113fae7f68bd69c39a17d
therealsujji <sujithacrysis@gmail.com>
2020-03-20 03:58:49 +0530

    updated icns

--- lib/presentation/custom_icons_icons.dart
+++ lib/presentation/custom_icons_icons.dart
@@ -27,4 +27,20 @@ class CustomIcons {
   static const IconData mobile_icon = IconData(0xe804, fontFamily: _kFontFam);
   static const IconData home = IconData(0xe805, fontFamily: _kFontFam);
   static const IconData inventory = IconData(0xe806, fontFamily: _kFontFam);
+  static const IconData egg = IconData(0xe807, fontFamily: _kFontFam);
+  static const IconData food__1_ = IconData(0xe808, fontFamily: _kFontFam);
+  static const IconData food = IconData(0xe809, fontFamily: _kFontFam);
+  static const IconData group_3661 = IconData(0xe80a, fontFamily: _kFontFam);
+  static const IconData ketogenic_diet = IconData(0xe80b, fontFamily: _kFontFam);
+  static const IconData milk = IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData peanut = IconData(0xe80d, fontFamily: _kFontFam);
+  static const IconData sesame = IconData(0xe80e, fontFamily: _kFontFam);
+  static const IconData shrimp = IconData(0xe80f, fontFamily: _kFontFam);
+  static const IconData soybean = IconData(0xe810, fontFamily: _kFontFam);
+  static const IconData soy_free = IconData(0xe811, fontFamily: _kFontFam);
+  static const IconData wheat__1_ = IconData(0xe812, fontFamily: _kFontFam);
+  static const IconData wheat = IconData(0xe813, fontFamily: _kFontFam);
+  static const IconData chicken = IconData(0xe814, fontFamily: _kFontFam);
+  static const IconData dairy_product = IconData(0xe815, fontFamily: _kFontFam);
+  static const IconData dolly_fish = IconData(0xe816, fontFamily: _kFontFam);
 }
